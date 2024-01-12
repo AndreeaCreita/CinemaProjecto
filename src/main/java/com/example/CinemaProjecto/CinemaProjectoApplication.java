98d52ec84543dd2e85d97e9e877c5d5a239303a1
Andreea Creita <andreea_creita@yahoo.com>
2024-01-12 22:22:51 +0200

    first commit - creare proiect

---
+++ src/main/java/com/example/CinemaProjecto/CinemaProjectoApplication.java
@@ -0,0 +1,13 @@
+package com.example.CinemaProjecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaProjectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaProjectoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CinemaProjecto/CinemaProjectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CinemaProjecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaProjectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

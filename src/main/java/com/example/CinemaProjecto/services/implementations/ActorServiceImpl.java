cb4da88594b432863238054d6ecf96aa41cdbf5b
Andreea Creita <andreea_creita@yahoo.com>
2024-05-08 14:18:50 +0300

    log done

--- src/main/java/com/example/CinemaProjecto/services/implementations/ActorServiceImpl.java
+++ src/main/java/com/example/CinemaProjecto/services/implementations/ActorServiceImpl.java
@@ -7,10 +7,11 @@ import com.example.CinemaProjecto.repositories.ActorRepository;
 import com.example.CinemaProjecto.repositories.MovieRepository;
 import com.example.CinemaProjecto.services.ActorService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ActorServiceImpl implements ActorService {
@@ -20,9 +21,17 @@ public class ActorServiceImpl implements ActorService {
     public List<ActorDto> getActorsByMovie(Long movieId) {
         var movie = movieRepository.findById(movieId).orElseThrow(() ->
                 new NotFoundException("Movie not found"));
-        return actorRepository.findByMovie(movieId).stream().map(
-            a -> new ActorDto(a.getId(), a.getName(),
-                    a.getMovies().stream().map(Movie::getTitle).toList())
-        ).toList();
+
+        log.info("Retrieved movie for actor processing, Movie ID: " + movie.getId() + ", Movie Title: " + movie.getTitle());
+
+        return actorRepository.findByMovie(movieId).stream().map(a -> {
+            log.info("Processing actor, Actor ID: " + a.getId() + ", Actor Name: " + a.getName());
+
+            return new ActorDto(
+                    a.getId(),
+                    a.getName(),
+                    a.getMovies().stream().map(Movie::getTitle).toList()
+            );
+        }).toList();
     }
 }
--- src/main/java/com/example/CinemaProjecto/services/implementations/CinemaServiceImpl.java
+++ src/main/java/com/example/CinemaProjecto/services/implementations/CinemaServiceImpl.java
@@ -6,10 +6,11 @@ import com.example.CinemaProjecto.models.Cinema;
 import com.example.CinemaProjecto.repositories.CinemaRepository;
 import com.example.CinemaProjecto.services.CinemaService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CinemaServiceImpl implements CinemaService {
@@ -19,6 +20,7 @@ public class CinemaServiceImpl implements CinemaService {
     public CinemaDto getById(Long id) {
         Cinema cinema = repository.findById(id).orElseThrow(() ->
                 new NotFoundException("Cinema not found"));
+        log.info("Retrieved cinema, ID: " + cinema.getId() + ", Name: " + cinema.getName());
         return new CinemaDto(
                 cinema.getId(),
                 cinema.getName(),
@@ -28,11 +30,14 @@ public class CinemaServiceImpl implements CinemaService {
 
     @Override
     public List<CinemaDto> getAll() {
-        return repository.findAll().stream().map(c ->
-                new CinemaDto(
-                        c.getId(),
-                        c.getName(),
-                        c.getLocation()
-                )).toList();
+        return repository.findAll().stream().map(c -> {
+            log.info("Processing cinema, ID: " + c.getId() + ", Name: " + c.getName());
+
+            return new CinemaDto(
+                    c.getId(),
+                    c.getName(),
+                    c.getLocation()
+            );
+        }).toList();
     }
 }

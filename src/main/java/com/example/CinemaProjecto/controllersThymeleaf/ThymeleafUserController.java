86e31409cf9877ecd598001ecc1392d929e5447f
Andreea Creita <andreea_creita@yahoo.com>
2024-05-10 16:23:58 +0300

    Thymeleaf components added: User Thymeleaf Controller + user.html working fine + error.html + user-not-found.html Main Page in progress + main.html

---
+++ src/main/java/com/example/CinemaProjecto/controllersThymeleaf/ThymeleafUserController.java
@@ -0,0 +1,30 @@
+package com.example.CinemaProjecto.controllersThymeleaf;
+import com.example.CinemaProjecto.dtos.UserDto;
+import com.example.CinemaProjecto.services.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+@RequiredArgsConstructor
+public class ThymeleafUserController {
+
+    private final UserService userService;
+
+    @GetMapping("/users/view/{userId}")
+    public String getUserPage(@PathVariable Long userId, Model model) {
+        try {
+            UserDto user = userService.getUser(userId);
+            if (user == null) {
+                return "user-not-found";
+            }
+            model.addAttribute("user", user);
+            return "user";  // Directs to the user.html Thymeleaf template
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "An error occurred while trying to fetch user details.");
+            return "error";  // Directs to the error.html Thymeleaf template
+        }
+    }
+}
